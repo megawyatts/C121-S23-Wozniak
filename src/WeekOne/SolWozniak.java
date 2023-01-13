2a6aea476f49adef1f035dd63bac0a93cee9a9c4
megawyatts <sarasaurus101@gmail.com>
2023-01-13 09:41:56 -0500

    Week 1

---
+++ src/WeekOne/SolWozniak.java
@@ -0,0 +1,22 @@
+package WeekOne;
+
+public class SolWozniak {
+    public static void main(String[] args){
+        int age;
+        double gpa;
+        char letterGrade;
+        boolean csMajor;
+
+        //initialize
+        age = 19;
+        gpa= 3.9;
+        letterGrade='B';
+        csMajor=false;
+
+        String favoriteAColor;
+        favoriteAColor="periwinkle";
+
+        System.out.println("Age:"+age+" GPA:"+gpa+" Grade:"+letterGrade+"\nComputer Science Major: "+csMajor+" Favorite color:"+favoriteAColor);
+        }
+
+}
