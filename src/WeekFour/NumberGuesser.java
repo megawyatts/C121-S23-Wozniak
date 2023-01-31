6af28437489f359f536b6e89003dee1f4a9fec39
megawyatts <sarasaurus101@gmail.com>
2023-01-31 10:17:13 -0500

    Commmit

--- src/WeekFour/NumberGuesser.java
+++ src/WeekFour/NumberGuesser.java
@@ -7,19 +7,35 @@ public class NumberGuesser {
         Random random = new Random();
         int targetInt=random.nextInt(101);
         int userGuess;
+        String userInput;
+        int guessCount=1;
 
-        System.out.println("Give me a number between 1 and 100 and i'll tell you whether you're higher or lower than the number i'm thinking of.");
-        userGuess=Integer.parseInt(keyboard.nextLine());
-        while (userGuess!=targetInt){
-            if (userGuess<targetInt){
-                System.out.println("Your number was lower than my number. Try Again.");
-                userGuess=Integer.parseInt(keyboard.nextLine());
-            }else{
-                System.out.println("Your number was higher than my number. Try Again.");
-                userGuess=Integer.parseInt(keyboard.nextLine());
+        System.out.println("Give me a number between 1 and 100 and i'll tell you whether you're higher or lower than the number i'm thinking of.\nOr enter q to quit");
+        userInput=(keyboard.nextLine());
+        if (userInput.equals("q")){
+            userGuess=targetInt;
+        }else{
+            userGuess=Integer.parseInt(userInput);
+            while ((userGuess!=targetInt)){
+                if(userInput.equals("q")){
+                    System.out.println("You quit. The number was "+targetInt);
+                    break;
+                }else{
+                    userGuess=Integer.parseInt(userInput);
+                    if(userGuess==targetInt){
+                        break;
+                    }
+                    if (userGuess<targetInt){
+                        System.out.println("Your number was lower than my number. Try Again.\nOr enter q to quit.");
+                    }else{
+                        System.out.println("Your number was higher than my number. Try Again.\nOr enter q to quit.");
+                    }
             }
-        }
-        System.out.printf("You're correct! My number was "+targetInt+"! Thank you for playing.");
+                guessCount++;
+                userInput=(keyboard.nextLine());
+        }}
+        if(userGuess==targetInt){
+            System.out.printf("You're correct! My number was "+targetInt+"! It took you "+ guessCount+" guesses to get it right!\nThank you for playing.");}
 
     }
 }
